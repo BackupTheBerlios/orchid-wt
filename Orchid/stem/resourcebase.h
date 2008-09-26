fc8b7746d52df541fcacd7aa7243d095d0eade94
Johannes Bergmeier <josel@josel-laptop.(none)>
2008-09-26 02:39:14 +0200

    - Added copyright and license notices to the source files

--- Orchid/stem/resourcebase.h
+++ Orchid/stem/resourcebase.h
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the Orchid libraries
+ *
+ * Copyright (C) 2008 Johannes Bergmeier
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef _ORCHID_RESOURCEBASE_H_
 #define _ORCHID_RESOURCEBASE_H_
 
